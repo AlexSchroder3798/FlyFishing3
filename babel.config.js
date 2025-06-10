daca00b8ad47a7689510e8b7771467d8249ea460
AlexSchroder3798 <alex.schroder.pr@gmail.com>
2025-06-10 13:13:29 -0400

    Fix Babel configuration for Hermes compatibility

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [],   // Removed 'expo-router/babel' as it's deprecated in SDK 50+
+    plugins: ['react-native-reanimated/plugin'],   // Added react-native-reanimated plugin for Hermes compatibility
   };
 };
